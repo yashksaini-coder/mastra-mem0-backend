6cc3f504262f06ceb32095f76d0036742b0557a6
yashksaini-coder <ys3853428@gmail.com>
2025-04-15 15:48:13 +0530

    Add Vercel deployer integration and update dependencies; include .vercel in .gitignore

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -3,9 +3,15 @@ import { Mastra } from '@mastra/core/mastra';
 import { createLogger } from '@mastra/core/logger';
 import { GithubAgent, EmailAgent, editorAgent, mem0Agent } from './agents';
 import mem0Workflow from './workflows/index';
+import { VercelDeployer } from '@mastra/deployer-vercel';
 export const mastra = new Mastra({
   agents: { GithubAgent, EmailAgent, editorAgent, mem0Agent },
   workflows: { mem0Workflow },
+  deployer: new VercelDeployer({
+    teamSlug: 'yash-kumar-sainis-projects',
+    projectName: 'mem0-mastra-backend',
+    token: 'KtEjUieZeke3A0MtgxDQLtb4',
+  }),
   logger: createLogger({
     name: 'Mastra',
     level: 'info',
